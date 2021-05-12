bacd568c2ba2a0ae39adaba603fd65a75e3bc9c4
Rodrigo Sousa <rod_sousa@hotmail.com>
2021-05-12 20:09:58 +0100

    Added script: RBF Still needs to be checked if working propperly

---
+++ rbf/src/lib.rs
@@ -0,0 +1,133 @@
+use nalgebra::{DMatrix};
+
+pub type Element = f64;
+pub type Matrix = DMatrix<Element>;
+
+pub struct RBF {
+    number_points: usize,
+    number_clusters: usize,
+    x: Vec<Matrix>,
+    targets: Matrix,
+    initial_weights: Matrix,
+    learning_rate: Element,
+    cluster_centers: Vec<Matrix>,
+    cluster_sigmas: Vec<Element>,
+}
+
+impl RBF {
+
+    pub fn do_n_iterations(&self, iterations: usize) {
+        // Compute Radial Layer Matrix
+        let radial_matrix = self.compute_radial_matrix();
+        println!("Radial Matrix = Φ = {}", radial_matrix);
+
+        // Split Radial Matrix by Row
+        let radial_matrix_split = split_matrix_by_row(radial_matrix, self.number_points, self.number_clusters + 1);
+        for line in radial_matrix_split.iter() { println!("{}", line); }
+
+        // Store weights
+        let mut weights = self.initial_weights.clone();
+        println!("Weights at 0: {}", weights);
+
+        // Do N iterations to the weights
+        // TODO: Implement more than cross-entropy with sigmoid function
+        for iteration in 0..iterations {
+            weights = cross_entropy_sigmoid_update(weights, radial_matrix_split.clone(), self.targets.clone(), self.learning_rate);
+            println!("Weights at {}: {}", iteration + 1, weights);
+        }
+    }
+
+    pub fn compute_radial_matrix(&self) -> Matrix {
+
+        let mut radial_values : Vec<Element> = Vec::new();
+
+        // Add bias column values
+        for _ in 0..self.number_points { radial_values.push(1.0) }
+        // Add values column by column
+        for (cluster_center, &cluster_sigma) in self.cluster_centers.iter().zip(self.cluster_sigmas.iter()) {
+            for point in self.x.iter() {
+                let new_radial_value = radial_function(point.clone(), cluster_center.clone(), cluster_sigma);
+                radial_values.push(new_radial_value);
+            }
+        }
+
+        let radial_matrix = DMatrix::from_vec(self.number_points, self.number_clusters + 1, radial_values);
+        return radial_matrix;
+    }
+}
+
+pub fn build_rbf(number_points: usize, number_clusters: usize, x: Vec<Matrix>, targets: Matrix, initial_weights: Matrix, learning_rate: Element, cluster_centers: Vec<Matrix>, cluster_sigmas: Vec<Element>) -> RBF {
+    let new_rbf : RBF = RBF {
+        number_points: number_points,
+        number_clusters: number_clusters,
+        x: x,
+        targets: targets,
+        initial_weights: initial_weights,
+        learning_rate: learning_rate,
+        cluster_centers: cluster_centers,
+        cluster_sigmas: cluster_sigmas,
+    };
+
+    return new_rbf;
+}
+
+pub fn radial_function(x: Matrix, cluster_center: Matrix, cluster_sigma: Element) -> Element {
+    
+    // Compute distance
+    let mut distance = 0.0;
+    for (&x_value, &cluster_center_value) in x.iter().zip(cluster_center.iter()) {
+        distance = distance + (x_value - cluster_center_value).powi(2);
+    }
+
+    // Compute radial value
+    let denominator = 2.0 * cluster_sigma.powi(2);
+    let radial_value = (- distance / denominator).exp();
+
+    return radial_value;
+}
+
+pub fn cross_entropy_sigmoid_update(previous_weights: Matrix, radial_matrix: Vec<Matrix>, targets: Matrix, learning_rate: Element) -> Matrix {
+    
+    let mut new_weights = previous_weights.clone();
+    for (point, &target) in radial_matrix.iter().zip(targets.iter()) {
+
+        let current_computation = learning_rate * point * (target - sigmoid(previous_weights.clone(), point.clone()));
+        new_weights = new_weights + current_computation;
+    }
+
+    return new_weights;
+}
+
+pub fn sigmoid(weights: Matrix, point: Matrix) -> Element {
+
+    // Multiply weights by point
+    let mut value = 0.0;
+    for (&weight, &point_value) in weights.iter().zip(point.iter()) {
+        value = value + weight * point_value;
+    }
+
+    // Aplly sigmoid function
+    value = 1.0 / (1.0 + (-value).exp());
+
+    return value;
+}
+
+fn split_matrix_by_row(matrix: Matrix, number_rows: usize, number_columns: usize) -> Vec<Matrix> {
+
+    let mut final_result : Vec<Matrix> = Vec::new();
+
+    let mut temp_values : Vec<Vec<Element>> = Vec::new();
+    for _ in 0..number_rows { temp_values.push(Vec::new()); }
+
+    for (index, &value) in matrix.iter().enumerate() {
+        let row = index % number_rows;
+        temp_values[row].push(value);
+    }
+
+    for line in temp_values {
+        let sub_matrix = DMatrix::from_vec(number_columns, 1, line);
+        final_result.push(sub_matrix);
+    }
+
+    return final_result;
+}
---
+++ rbf/src/main.rs
@@ -0,0 +1,151 @@
+use nalgebra::{DMatrix};
+use std::io::{self, Write};
+
+mod lib;
+
+fn main() {
+
+    // Number of iterations
+    let mut line : &str = "How many iterations do you want computed?";
+    let buffer = ask_for_input(line);
+    let iterations : usize = buffer.trim().parse().unwrap();
+
+    // Get dimension
+    line = "What dimension are we dealing with?";
+    let buffer = ask_for_input(line);
+    let dimension : usize = buffer.trim().parse().unwrap();
+    
+    // Get number of points
+    line = "What is the number of points given?";
+    let buffer = ask_for_input(line);
+    let number_points : usize = buffer.trim().parse().unwrap();
+    
+    // Get number of clusters
+    line = "What is the number of clusters that we are dealing with?";
+    let buffer = ask_for_input(line);
+    let number_clusters : usize = buffer.trim().parse().unwrap();
+
+    
+    println!();
+
+    let x = load_x(number_points as i32, dimension);
+    println!();
+    let targets = load_targets(number_points as i32);
+    println!();
+    let initial_weights = load_weights(number_clusters as i32);
+    println!();
+    let learning_rate = load_learning_rate();
+    println!();
+    let cluster_centers = load_cluster_centers(number_clusters as i32, dimension);
+    println!();
+    let cluster_sigmas = load_cluster_sigmas(number_clusters as i32);
+    println!();
+
+    let problem : lib::RBF = lib::build_rbf(number_points, number_clusters, x, targets, initial_weights, learning_rate, cluster_centers, cluster_sigmas);
+    problem.do_n_iterations(iterations);
+}
+
+pub fn ask_for_input(line: &str) -> String {
+    println!("{}", line);
+    print!("> ");
+    
+    let stdin : std::io::Stdin = io::stdin();
+    let mut buffer : String = String::new();
+    let _ = io::stdout().flush();
+    stdin.read_line(&mut buffer).unwrap();
+
+    return buffer;
+}
+
+pub fn load_x(number_points: i32, dimension: usize) -> Vec<lib::Matrix> {
+
+    let mut points : Vec<lib::Matrix> = Vec::new();    
+    for _ in 0..number_points {
+
+        let line = "What is the next point?";
+        let input = ask_for_input(line);
+
+        // Process input line
+        let values_vec = input.trim().split_whitespace()
+            .map(|value| value.parse().unwrap()).collect::<Vec<lib::Element>>();
+
+        let value = DMatrix::from_vec(dimension, 1, values_vec);
+        points.push(value);
+    }
+
+    return points;
+}
+
+pub fn load_targets(number_points: i32) -> lib::Matrix {
+
+    let line = "What are the targets for the points (all together)?";
+    let input = ask_for_input(line);
+
+    // Process input line
+    let values_vec = input.trim().split_whitespace()
+        .map(|value| value.parse().unwrap()).collect::<Vec<lib::Element>>();
+
+    let value = DMatrix::from_vec(number_points as usize, 1, values_vec);
+
+    return value;
+}
+
+pub fn load_weights(number_clusters: i32) -> lib::Matrix {
+
+    let line = "What is the initial weight matrix?";
+    let input = ask_for_input(line);
+
+    // Process input line
+    let values_vec = input.trim().split_whitespace()
+        .map(|value| value.parse().unwrap()).collect::<Vec<lib::Element>>();
+
+    let value = DMatrix::from_vec((number_clusters + 1) as usize, 1, values_vec);
+
+    return value;
+}
+
+pub fn load_learning_rate() -> lib::Element {
+
+    let line = "What is the learning rate?";
+    let input = ask_for_input(line);
+
+    // Process input line
+    let value = input.trim().parse().unwrap();
+
+    return value;
+}
+
+pub fn load_cluster_centers(number_clusters: i32, dimension: usize) -> Vec<lib::Matrix> {
+
+    let mut points : Vec<lib::Matrix> = Vec::new();    
+    for _ in 0..number_clusters {
+
+        let line = "What is the next cluster?";
+        let input = ask_for_input(line);
+
+        // Process input line
+        let values_vec = input.trim().split_whitespace()
+            .map(|value| value.parse().unwrap()).collect::<Vec<lib::Element>>();
+
+        let value = DMatrix::from_vec(dimension, 1, values_vec);
+        points.push(value);
+    }
+
+    return points;
+}
+
+pub fn load_cluster_sigmas(number_clusters: i32) -> Vec<lib::Element> {
+
+    let mut points : Vec<lib::Element> = Vec::new();    
+    for _ in 0..number_clusters {
+
+        let line = "What is the sigma for the next cluster?";
+        let input = ask_for_input(line);
+
+        // Process input line
+        let value = input.trim().parse().unwrap();
+        points.push(value);
+    }
+
+    return points;
+}
